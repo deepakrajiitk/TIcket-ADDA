91543841d2b942700bdf26e8209785784baf6c13
dinkart <dinkartewary8@gmail.com>
2023-04-15 02:14:59 +0530

    minor

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
-import Home from "./pages/home"
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
+
+import Home from "./pages/home";
+import Show from "./pages/loginTwo";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/search">
+          <Show />
+        </Route>
+
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
