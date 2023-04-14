a6b05c5e30623f8f15851c5a890c5b9ec19037ac
deepakrajiitk <deepakrj128@gmail.com>
2023-04-14 21:01:23 +0530

    added reactjs

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import AboutPage from "./components/AboutPage";
+import HomePage from "./components/HomePage";
+import MyNavbar from "./components/MyNavBar";
+
+function App() {
+  return (
+    <>
+      <MyNavbar />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/about" element={<AboutPage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
