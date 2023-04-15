435b01d9029c183a40a448af76649c1fe87cc709
syncOctal <ashupatel378@gmail.com>
2023-04-15 23:38:39 +0530

    ticket files updated

--- client/src/pages/TicketList.js
+++ client/src/pages/TicketList.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './style_ticket.css'
 
 
 const handleBuyTicket = (ticketId) => {
     // handle logic to buy ticket with the given ticketId
-
   }
 
 function Table(props) {
@@ -23,7 +22,7 @@ function Table(props) {
           <td>{row.tktPrice}</td>
 
           <td>
-            <button onClick={() => handleBuyTicket(row.id)}>Buy Ticket</button>
+            <button onClick={() => handleBuyTicket(row.id)}>Buy</button>
           </td>
 
           <td>
@@ -41,11 +40,12 @@ function Table(props) {
 class TicketList extends Component {
   render() {
     return (
+
       <section class="ftco-section">
 		<div class="container">
 			<div class="row justify-content-center">
 				<div class="col-md-6 text-center mb-5">
-					<h2 class="heading-section">Tickets Available for This Route</h2>
+					<h2 class="heading-section">TICKETS AVAILABLE FOR THIS ROUTE</h2>
 				</div>
 			</div>
 			<div class="row">
@@ -62,6 +62,7 @@ class TicketList extends Component {
                                     <th>Departure Time</th>
                                     <th>Ticket Price</th>
                                 <th>Buy Ticket</th>
+                                <th/>
                                 </tr>
 						  </thead>
 
